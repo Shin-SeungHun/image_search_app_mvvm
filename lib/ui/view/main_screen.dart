0e99b2b3e773dfc0d2db327dd2bac01541d171a0
shin-SeungHun <sng7387@gmail.com>
2024-01-02 17:48:59 +0900

    viewModel로 ui와 로직 분리

--- lib/ui/view/main_screen.dart
+++ lib/ui/view/main_screen.dart
@@ -1,44 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app_mvvm/data/model/image_item.dart';
+import 'package:image_search_app_mvvm/view_model/main_view_model.dart';
+import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final TextEditingController searchTextEditingController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    searchTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final MainViewModel viewModel = context.watch<MainViewModel>();
     return Scaffold(
       appBar: AppBar(
         leading: const FlutterLogo(),
-        title: const Text('image search app'),
+        title: const Text('image searching app'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: '검색',
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  width: 2,
-                  color: Colors.green,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: searchTextEditingController,
+                onChanged: (query) {
+                  viewModel.searchImage(query);
+                },
+                decoration: InputDecoration(
+                  labelText: '검색',
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.cyan,
+                    ),
+                    onPressed: () {
+                      viewModel.searchImage(searchTextEditingController.text);
+                    },
+                  ),
                 ),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  width: 2,
-                  color: Colors.green,
-                ),
-              ),
-              suffixIcon: IconButton(
-                icon: const Icon(
-                  Icons.search,
-                  color: Colors.cyan,
-                ),
-                onPressed: () {},
-              ),
-            ),
-          )
-        ],
+              const SizedBox(height: 24),
+              Expanded(
+                child: GridView.builder(
+                    itemCount: viewModel.imageItems.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 32,
+                            mainAxisSpacing: 32),
+                    itemBuilder: (context, index) {
+                      final ImageItem imageItem = viewModel.imageItems[index];
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.network(
+                          imageItem.imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
