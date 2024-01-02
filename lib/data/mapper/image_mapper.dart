237975de9e27fa8164ba6a70e4b53681f1b77908
shin-SeungHun <sng7387@gmail.com>
2024-01-02 23:25:20 +0900

    mapper 수정

--- lib/data/mapper/image_mapper.dart
+++ lib/data/mapper/image_mapper.dart
@@ -8,6 +8,7 @@ extension DtoToModel on Hits {
       imageUrl: previewURL ??
           'https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90',
       tags: tags ?? '',
+      id: id ?? 0,
     );
   }
 }
