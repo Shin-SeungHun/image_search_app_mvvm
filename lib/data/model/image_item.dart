e0cb6e2d82d650851b4657c177c670814eae0765
shin-SeungHun <sng7387@gmail.com>
2024-01-02 23:19:53 +0900

    model에 id추가

--- lib/data/model/image_item.dart
+++ lib/data/model/image_item.dart
@@ -1,9 +1,11 @@
 class ImageItem {
+  num id;
   String imageUrl;
   String tags;
 
 //<editor-fold desc="Data Methods">
   ImageItem({
+    required this.id,
     required this.imageUrl,
     required this.tags,
   });
@@ -13,38 +15,43 @@ class ImageItem {
       identical(this, other) ||
       (other is ImageItem &&
           runtimeType == other.runtimeType &&
+          id == other.id &&
           imageUrl == other.imageUrl &&
           tags == other.tags);
 
   @override
-  int get hashCode => imageUrl.hashCode ^ tags.hashCode;
+  int get hashCode => id.hashCode ^ imageUrl.hashCode ^ tags.hashCode;
 
   @override
   String toString() {
-    return 'ImageItem{ imageUrl: $imageUrl, tags: $tags,}';
+    return 'ImageItem{ id: $id, imageUrl: $imageUrl, tags: $tags,}';
   }
 
   ImageItem copyWith({
+    num? id,
     String? imageUrl,
     String? tags,
   }) {
     return ImageItem(
+      id: id ?? this.id,
       imageUrl: imageUrl ?? this.imageUrl,
       tags: tags ?? this.tags,
     );
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'imageUrl': imageUrl,
       'tags': tags,
     };
   }
 
-  factory ImageItem.fromMap(Map<String, dynamic> map) {
+  factory ImageItem.fromJson(Map<String, dynamic> json) {
     return ImageItem(
-      imageUrl: map['imageUrl'] as String,
-      tags: map['tags'] as String,
+      id: json['id'] as num,
+      imageUrl: json['imageUrl'] as String,
+      tags: json['tags'] as String,
     );
   }
 
