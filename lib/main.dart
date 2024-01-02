1ffc21049faf4db0305e431c1e31a238a59a8c29
shin-SeungHun <sng7387@gmail.com>
2024-01-02 17:37:20 +0900

    상태관리 위해 ChangeNotifierProvider 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_search_app_mvvm/ui/view/main_screen.dart';
+import 'package:image_search_app_mvvm/view_model/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: ChangeNotifierProvider.value(
+          value: MainViewModel(),
+          child: const MainScreen(),
+        ));
   }
 }
-
