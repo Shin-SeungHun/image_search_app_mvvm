10ddea03186def33e2cd83cdbad4836ec402f857
shin-SeungHun <sng7387@gmail.com>
2024-01-04 07:20:02 +0900

    renamed

--- lib/data/repository/image_item_repository_impl.dart
+++ lib/data/repository/image_item_repository_impl.dart
@@ -2,10 +2,10 @@ import 'package:image_search_app_mvvm/data/data_source/pixabay_api.dart';
 import 'package:image_search_app_mvvm/data/dto/pixabay_dto.dart';
 import 'package:image_search_app_mvvm/data/mapper/image_mapper.dart';
 import 'package:image_search_app_mvvm/data/model/image_item.dart';
-import 'package:image_search_app_mvvm/data/repository/interface/image_item_repository_impl.dart';
+import 'package:image_search_app_mvvm/data/repository/interface/image_item_repository.dart';
 
-class PixabayImageItemRepository implements ImageItemRepository {
-  final _api = PixabayApi();
+class ImageItemRepositoryImpl implements ImageItemRepository {
+  final PixabayApi _api = PixabayApi();
 
   @override
   Future<List<ImageItem>> getImageItems(String query) async {
