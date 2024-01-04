ffe0f91f57da5e67a4a63d78ccbb7b8c03426fa6
shin-SeungHun <sng7387@gmail.com>
2024-01-05 00:30:58 +0900

    viewModel에 에러처리 적용

--- lib/view_model/main_view_model.dart
+++ lib/view_model/main_view_model.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_search_app_mvvm/core/result.dart';
 import 'package:image_search_app_mvvm/data/model/image_item.dart';
 import 'package:image_search_app_mvvm/data/repository/image_item_repository_impl.dart';
 
@@ -11,7 +14,6 @@ class MainViewModel extends ChangeNotifier {
   MainState _state = const MainState();
 
   MainState get state => _state;
-
 
   MainViewModel({
     required ImageItemRepository repository,
@@ -32,10 +34,30 @@ class MainViewModel extends ChangeNotifier {
     final results = (await _repository.getImageItems(query));
 
     // 화면갱신
-    _state = state.copyWith(
-      isLoading: false,
-      imageItems: results,
-    );
-    notifyListeners();
+    switch (results) {
+      case Success<List<ImageItem>>():
+        _state = state.copyWith(
+          isLoading: false,
+          imageItems: results.data,
+        );
+        if (kDebugMode) {
+          print('성공');
+        }
+        notifyListeners();
+      case Error<List<ImageItem>>():
+        _state = state.copyWith(
+          isLoading: false,
+        );
+        notifyListeners();
+        if (kDebugMode) {
+          print('실패 ${results.e.toString()}');
+        }
+      case Loading<List<ImageItem>>():
+      // TODO: 로딩
+        if(kDebugMode){
+          print('loading');
+        }
+        const CircularProgressIndicator();
+    }
   }
 }
