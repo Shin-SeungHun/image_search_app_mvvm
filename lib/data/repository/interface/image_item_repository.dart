6c121ebb2027c764ebfe3c9fbb35881dbfedf30e
shin-SeungHun <sng7387@gmail.com>
2024-01-05 00:15:19 +0900

    result 클래스로 매핑

--- lib/data/repository/interface/image_item_repository.dart
+++ lib/data/repository/interface/image_item_repository.dart
@@ -1,5 +1,6 @@
+import 'package:image_search_app_mvvm/core/result.dart';
 import 'package:image_search_app_mvvm/data/model/image_item.dart';
 
 abstract interface class ImageItemRepository {
-  Future<List<ImageItem>> getImageItems(String query);
+  Future<Result<List<ImageItem>>> getImageItems(String query);
 }
