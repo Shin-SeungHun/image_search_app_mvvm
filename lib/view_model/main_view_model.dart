d58b159da7299525561899cb262908495a455489
shin-SeungHun <sng7387@gmail.com>
2024-01-05 01:38:18 +0900

    refactor: 화면 갱신 로직 작성

--- lib/view_model/main_view_model.dart
+++ lib/view_model/main_view_model.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_search_app_mvvm/core/result.dart';
 import 'package:image_search_app_mvvm/data/model/image_item.dart';
 import 'package:image_search_app_mvvm/data/repository/image_item_repository_impl.dart';
+import 'package:image_search_app_mvvm/ui/event/main_event.dart';
 
 import '../data/repository/interface/image_item_repository.dart';
 import '../ui/state/main_state.dart';
@@ -14,6 +17,10 @@ class MainViewModel extends ChangeNotifier {
   MainState _state = const MainState();
 
   MainState get state => _state;
+
+  final _eventController = StreamController<MainEvent>();
+
+  Stream<MainEvent> get eventStream => _eventController.stream;
 
   MainViewModel({
     required ImageItemRepository repository,
@@ -27,6 +34,11 @@ class MainViewModel extends ChangeNotifier {
   }
 
   Future<void> searchImage(String query) async {
+    if (query.isEmpty) {
+      // 검색어가 비어 있을 때 스낵바 표시
+      _eventController.add(const MainEvent.showSnackBar('검색어를 입력하세요.'));
+      return;
+    }
     // 화면갱신
     _state = state.copyWith(isLoading: true);
     notifyListeners();
@@ -44,20 +56,28 @@ class MainViewModel extends ChangeNotifier {
           print('성공');
         }
         notifyListeners();
+        _eventController.add(const MainEvent.showSnackBar('성공'));
+        break;
       case Error<List<ImageItem>>():
         _state = state.copyWith(
           isLoading: false,
         );
         notifyListeners();
+        // CircularProgressIndicator();
+        //_eventController.add(MainEvent.showSnackBar(results.e.toString()));
+        //_eventController.add(MainEvent.dialog(results.e.toString()));
+        _eventController.add(const MainEvent.dialog('네트워크 에러'));
         if (kDebugMode) {
           print('실패 ${results.e.toString()}');
         }
+        break;
       case Loading<List<ImageItem>>():
-      // TODO: 로딩
-        if(kDebugMode){
+        // TODO: 로딩
+        if (kDebugMode) {
           print('loading');
         }
         const CircularProgressIndicator();
+        break;
     }
   }
 }
